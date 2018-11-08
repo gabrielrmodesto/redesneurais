b289f2b5f80ef96c65e3149ded6e2444ea1f6714
GabrielRM <gabrielrmodesto5@gmail.com>
2018-11-08 10:27:29 -0200

    Leitura

--- perceptron.py
+++ perceptron.py
@@ -1,6 +1,7 @@
 import csv
-# passo 3 inicializar os pesos dos valores entre -1 e +1
-# passo 4 loop treinamento
+import random
+
+
 # for(t=1;t<=150;i++) selecionar um par de treino
 #   linha = aleatorio()%150
 def main():
@@ -10,15 +11,26 @@ def main():
     except IOError as erro:
         print("Erro na leitura %s" % erro)
 
-    dados = []
-    line = []
-    j = 0
+    lista_classe = []
     #passo 2 altera valores classe
     for [x1,x2,x3,x4,classe] in file:
-        if classe is not '1':
-            classe = '-1'
+        classe = int(classe)
+        if classe is not 1:
+            classe = -1
 
-        print(classe)
-        
+        print(x1,x2,x3,x4,classe)
+        lista_classe.append(classe)
+
+    # passo 3 inicializar os pesos dos valores entre -1 e +1
+    lista_pesos = []
+    for i in range(4):
+        lista_pesos.append(random.randrange(-1,2,1))
+
+    print(lista_pesos)
+
+    # passo 4 loop treinamento
+    
+ 
+
 if __name__ == '__main__':
     main()
